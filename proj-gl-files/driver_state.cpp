9d9084ccdc3c39ed95c784adb3f51728f1d33ea5
West-Ben <benjamin.west.217@gmail.com>
2019-02-19 13:00:15 -0800

    lab portion completed

--- proj-gl-files/driver_state.cpp
+++ proj-gl-files/driver_state.cpp
@@ -1,5 +1,6 @@
 #include "driver_state.h"
 #include <cstring>
+#include <stdlib.h>
 
 driver_state::driver_state()
 {
@@ -16,11 +17,19 @@ driver_state::~driver_state()
 // are not known when this class is constructed.
 void initialize_render(driver_state& state, int width, int height)
 {
-    state.image_width=width;
-    state.image_height=height;
-    state.image_color=0;
+    state.image_width = width;
+    state.image_height = height;
+   
+    state.image_color = new pixel[width * height];
+    
+    
+    for (size_t i = 0; i < width * height ; i++)
+    {    	
+	    state.image_color[i] = make_pixel(0,0,0);
+    }
+
     state.image_depth=0;
-    std::cout<<"TODO: allocate and initialize state.image_color and state.image_depth."<<std::endl;
+    //std::cout<<"TODO: allocate and initialize state.image_color and state.image_depth."<<std::endl;
 }
 
 // This function will be called to render the data that has been stored in this class.
@@ -32,7 +41,40 @@ void initialize_render(driver_state& state, int width, int height)
 //   render_type::strip -    The vertices are to be interpreted as a triangle strip.
 void render(driver_state& state, render_type type)
 {
-    std::cout<<"TODO: implement rendering."<<std::endl;
+    //std::cout<<"TODO: implement rendering."<<std::endl;
+
+    switch (type)
+    {
+	case render_type::triangle:
+    	{
+		data_vertex ver_data;
+	       ver_data.data = new float[MAX_FLOATS_PER_VERTEX];
+		const data_geometry *g[3];
+		data_geometry g_ar[3];
+		data_geometry geo[3];
+		for (size_t i = 0; i < 3 ; i++)
+		{
+  			
+			for (size_t j = 0; j < state.floats_per_vertex; j++)
+			{
+				ver_data.data[j] = state.vertex_data[(state.floats_per_vertex * i) + j];
+			}
+			state.vertex_shader(ver_data,geo[i],state.uniform_data);
+			geo[i].gl_Position /= geo[i].gl_Position[3];
+
+			g[i] = &geo[i];
+		}
+		rasterize_triangle(state,g);
+		//delete geo[];
+	}
+	case render_type::indexed:
+	{}
+	case render_type::fan:
+	{}
+	case render_type::strip:
+	{}
+    }
+
 }
 
 
@@ -56,6 +98,54 @@ void clip_triangle(driver_state& state, const data_geometry* in[3],int face)
 // fragments, calling the fragment shader, and z-buffering.
 void rasterize_triangle(driver_state& state, const data_geometry* in[3])
 {
-    std::cout<<"TODO: implement rasterization"<<std::endl;
+//    std::cout<<"TODO: implement rasterization"<<std::endl;
+
+   float ai = (((state.image_width/2) * in[0]->gl_Position[0]) + ((state.image_width/2) - 0.5));
+   float aj = (((state.image_height/2) * in[0]->gl_Position[1]) + ((state.image_height/2) - 0.5));
+
+   float bi =  (((state.image_width/2) * in[1]->gl_Position[0]) + ((state.image_width/2) - 0.5));
+   float bj = (((state.image_height/2) * in[1]->gl_Position[1]) + ((state.image_height/2) - 0.5));
+
+   float ci =  (((state.image_width/2) * in[2]->gl_Position[0]) + ((state.image_width/2) - 0.5));
+   float cj = (((state.image_height/2) * in[2]->gl_Position[1]) + ((state.image_height/2) - 0.5));
+
+   float area = 0.5 * (((bi * cj) - (ci * bj)) - 
+			((ai * cj) - (ci * aj)) +  
+			((ai * bj) - (bi * aj))); 
+
+    int xmin = std::min(std::min(ai,bi),ci);
+    int xmax = std::max(std::max(ai,bi),ci);
+    int ymin = std::min(std::min(aj,bj),cj);
+    int ymax = std::max(std::max(aj,bj),cj);
+    for (int i = xmin; i < xmax; i++)
+    {
+
+    	for (int j = ymin; j < ymax ; j++)
+	{
+    		float alpha = 0.5 * (((bi * cj) - (ci * bj)) - 
+			((i * cj) - (ci * j)) +  
+			((i * bj) - (bi * j))); 
+    		float bravo = 0.5 * (((i * cj) - (ci * j)) - 
+			((ai * cj) - (ci * aj)) +  
+			((ai * j) - (i * aj))); 
+    		float gamma = 0.5 * (((bi * j) - (i * bj)) - 
+			((ai * j) - (i * aj)) +  
+			((ai * bj) - (bi * aj))); 
+
+
+		alpha /= area;
+		bravo /= area;
+		gamma /= area;
+		
+		if (alpha >= 0 && bravo >= 0 && gamma >= 0 && gamma >= 0 && alpha + bravo + gamma <= 1.01)
+		{
+
+			int index = (state.image_width  * j) + i;
+			state.image_color[index] = make_pixel(255,255,255);
+		}
+	}
+    }
+
+
 }
 
